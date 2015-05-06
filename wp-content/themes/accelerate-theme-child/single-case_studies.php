56c8719c6f8c002d516263859e830d91e1fc72da
Dannie McClain <mcclain.dannie@gmail.com>
2015-05-06 17:16:23 -0500

    Updated css on ACF plugin

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -12,11 +12,43 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<article class="case-study">
+
+		<?php while ( have_posts() ) : the_post();
+			$services = get_field('services');
+			$client = get_field('client');
+			$link = get_field('site_link');
+			$image_1 = get_field('image_1');
+			$image_2 = get_field('image_2');
+			$image_3 = get_field('image_3'); ?>
+
+		<article class="case-study">
+			<aside class="case-study-sidebar">
+				<h2><?php the_title(); ?></h2>
+				<h6><?php echo $services; ?></h6>
+				<h6>Client: <?php echo $client; ?></h6>
+
 				<?php the_content(); ?>
-				</article>
-			<?php endwhile; // end of the loop. ?>
+
+				<p><strong><a href="<?php echo $link; ?>">Visit Live Site</a></strong></p>
+			</aside>
+
+			<div class="case-study-images">
+				<?php if($image_1) { ?>
+					<img src="<?php echo $image_1; ?>" />
+				<?php } ?>
+
+				<?php if($image_2) { ?>
+					<img src="<?php echo $image_2; ?>" />
+				<?php } ?>
+
+				<?php if($image_3) { ?>
+					<img src="<?php echo $image_3; ?>" />
+				<?php } ?>
+			</div>
+
+			</article>
+
+		<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
